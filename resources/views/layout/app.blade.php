09fbec2a725e18ebaaebe653605b346b702f70e4
nhat <phamminhnhat0208@gmail.com>
2023-02-18 16:43:54 +0700

    test

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -15,6 +15,7 @@
      src="https://kit.fontawesome.com/975a2f75a4.js"
      crossorigin="anonymous"
    ></script>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 <body>
      
@@ -114,7 +115,7 @@
                                     </li>
                                     <li class="">
                                       <span class="active"
-                                        ><a class="list-c active" href="">Payroll</a></span
+                                        ><a class="list-c active" href="/payroll">Payroll</a></span
                                       >
                                     </li>
                                     <li class="">
@@ -251,7 +252,55 @@
                   </div>
           </nav>
           <div class="container-fluid">
-
+            <div class="">
+              <div class="page">
+                <div>
+                  <div class="section-body">
+                    <div class="container-fluid">
+                      <div class="row">
+                        <div class="col-12">
+                          <div class="card">
+                            <div class="card-body">
+                              <div class="table-responsive">
+                                <table class="table">
+                                  <thead>
+                                    <tr>
+                                      <th scope="col">#</th>
+                                      <th scope="col">First</th>
+                                      <th scope="col">Last</th>
+                                      <th scope="col">Handle</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <th scope="row">1</th>
+                                      <td>Mark</td>
+                                      <td>Otto</td>
+                                      <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row">2</th>
+                                      <td>Jacob</td>
+                                      <td>Thornton</td>
+                                      <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row">3</th>
+                                      <td colspan="2">Larry the Bird</td>
+                                      <td>@twitter</td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
                Đây sẽ là nội dung của trang
                @yield('content')
           </div>
