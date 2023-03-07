68bbcf51758f1b9cdb5ff1f20099744bc9560f77
letien <lehuutien27022002@gmail.com>
2023-03-07 22:27:20 +0700

    bhxh

--- resources/views/baohiemxhs/dsbhxh.blade.php
+++ resources/views/baohiemxhs/dsbhxh.blade.php
@@ -64,31 +64,6 @@
         </div>
     </div>
 @endsection
-<script>
-    function searchBhxh() {
-    var input, filter, table, tr, td, i, txtValue, e, giatri, stt;
-    e = document.getElementById("chooseSearch");
-    giaTri = e.options[e.selectedIndex].text;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    if(giaTri == "Mã nhân viên"){
-      stt = 0;
-    }
-    if(giaTri == "Mã BHXH"){
-      stt = 1;
-    }
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[stt];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
-  }
-</script>
+@section('linkjs')
+    <script src="/js/ListAccEmployee/index.js"></script>
+@endsection
