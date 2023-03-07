fb4fc3b39f9368b18b6a77706ffae8a94b056e4b
Quang <ndquang191@gmail.com>
2023-03-07 11:43:53 +0700

    userhomepage

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,13 +8,26 @@
 <div class="container-fluid user_homepage">
      <div class="homepage-container">
           <a href="" class="user_box_item">
-               
+               <i class="bi bi-person-vcard"></i>
+               <span>Thông tin cá nhân</span>
           </a>
-          <a href="" class="user_box_item"></a>
-          <a href="" class="user_box_item"></a>
-          <a href="" class="user_box_item"></a>
-          <a href="" class="user_box_item"></a>
-          <a href="" class="user_box_item"></a>
+          <a href="" class="user_box_item">
+               <i class="bi bi-file-earmark"></i>
+               <span>Hợp đồng lao động</span>
+          </a>
+          <a href="" class="user_box_item">
+               <i class="bi bi-file-earmark"></i>
+               <span>Bảo hiểm xã hội</span>
+          </a>
+          <a href="" class="user_box_item">
+               <i class="bi bi-calendar2-plus"></i>
+               <span>Nghỉ phép</span>
+          </a>
+          <a href="" class="user_box_item">
+               <i class="bi bi-clipboard-data"></i>
+               <span>Đánh giá</span>
+
+          </a>
 
      </div>
 </div>
