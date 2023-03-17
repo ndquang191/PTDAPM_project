99cab5448cca24bd30a8b17b3356043862408251
DoTri0512 <dotri271@gmail.com>
2023-03-17 23:12:24 +0700

    first_commit

--- resources/views/hdlds/hdld_ds.blade.php
+++ resources/views/hdlds/hdld_ds.blade.php
@@ -32,75 +32,72 @@
                     <a href="{{route('createHDLD')}}" class="link_add_btn"> + Thêm</a>
                 </div>
             </div>
-            <div class="add_btn">
-                <a href="{{route('createHDLD')}}" class="link_add_btn"> + Thêm</a>
-            </div>
-        </div>
-        <div class="hdld_main_container">
-            <div class="input_search">
-                {{-- <form action=""> --}}
-                <input type="text" name="search" id="myInput" placeholder="Tìm kiếm ..." class="form_input" onkeyup="myFunction()">
-                {{-- </form> --}}
-                <a href="#" class="search_btn" onclick="myFunction()">
-                    <i class="bi bi-search icon_color_search"></i>
-                </a>
-            </div>
-            <table class="table" id="myTable">
-                <thead>
-                    <tr>
-                        <th scope="col">STT</th>
-                        <th scope="col">Mã nhân viên</th>
-                        <th scope="col">Loại hợp đồng</th>
-                        <th scope="col">Ngày ký</th>
-                        <th scope="col">Ngày bắt đầu</th>
-                        <th scope="col">Ngày kết thúc</th>
-                        <th scope="col">Địa điểm làm việc</th>
-                        <th scope="col">Chuyên môn</th>
-                        <th scope="col"></th>
-    
-                    </tr>
-                </thead>
-                <tbody>
-                    @if (count($contracts) == 0)
-                    <script>
-                        Swal.fire({
-                        position: 'center',
-                        icon: 'error',
-                        title: 'Không có bản ghi !',
-                        showConfirmButton: true,
-                        confirmButtonText: 'Đóng',
-                        timer: 3000
-                      })
-                      </script>
-                    @else 
-                      <?php $count = 0 ?>
-                        @foreach ($contracts as $contract)
-                      <?php $count ++ ?>
+            <div class="hdld_main_container">
+                <div class="input_search">
+                    {{-- <form action=""> --}}
+                    <input type="text" name="search" id="myInput" placeholder="Tìm kiếm ..." class="form_input" onkeyup="myFunction()">
+                    {{-- </form> --}}
+                    <a href="#" class="search_btn" onclick="myFunction()">
+                        <i class="bi bi-search icon_color_search"></i>
+                    </a>
+                </div>
+                <table class="table" id="myTable">
+                    <thead>
                         <tr>
-                            <td scope="row">{{$count}}</td>
-                            <td>{{$contract->MaNV}}</td>
-                            <td>{{$contract->LoaiHopDong}}</td>
-                            <td>{{$contract->NgayKi}}</td>
-                            <td>{{$contract->NgayBatDau}}</td>
-                            <td>{{$contract->NgayKetThuc}}</td>
-                            <td>{{$contract->DiaDiem}}</td>
-                            <td>{{$contract->ChuyenMon}}</td>
-                            <td>
-                                <a href="{{route('showDetailHDLD', ['id' => $contract->MaHDLD ])}}">
-                                    <i class="bi bi-eye-fill icon_color"></i>
-                                </a>
-                                {{-- <a href="{{route('editHDLD',['id' => 1])}}">
-                                    <i class="bi bi-pencil-square icon_color"></i>
-                                </a> --}}
-                                <a href="#">
-                                    <i class="bi bi-trash icon_color"></i>
-                                </a>
-                            </td>
-                        <tr>
-                        @endforeach
-                    @endif
-                </tbody>
-            </table>
+                            <th scope="col">STT</th>
+                            <th scope="col">Mã nhân viên</th>
+                            <th scope="col">Loại hợp đồng</th>
+                            <th scope="col">Ngày ký</th>
+                            <th scope="col">Ngày bắt đầu</th>
+                            <th scope="col">Ngày kết thúc</th>
+                            <th scope="col">Địa điểm làm việc</th>
+                            <th scope="col">Chuyên môn</th>
+                            <th scope="col"></th>
+        
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if (count($contracts) == 0)
+                        <script>
+                            Swal.fire({
+                            position: 'center',
+                            icon: 'error',
+                            title: 'Không có bản ghi !',
+                            showConfirmButton: true,
+                            confirmButtonText: 'Đóng',
+                            timer: 3000
+                          })
+                          </script>
+                        @else 
+                          <?php $count = 0 ?>
+                            @foreach ($contracts as $contract)
+                          <?php $count ++ ?>
+                            <tr>
+                                <td scope="row">{{$count}}</td>
+                                <td>{{$contract->MaNV}}</td>
+                                <td>{{$contract->LoaiHopDong}}</td>
+                                <td>{{$contract->NgayKi}}</td>
+                                <td>{{$contract->NgayBatDau}}</td>
+                                <td>{{$contract->NgayKetThuc}}</td>
+                                <td>{{$contract->DiaDiem}}</td>
+                                <td>{{$contract->ChuyenMon}}</td>
+                                <td>
+                                    <a href="{{route('showDetailHDLD', ['id' => $contract->MaHDLD ])}}">
+                                        <i class="bi bi-eye-fill icon_color"></i>
+                                    </a>
+                                    {{-- <a href="{{route('editHDLD',['id' => 1])}}">
+                                        <i class="bi bi-pencil-square icon_color"></i>
+                                    </a> --}}
+                                    <a href="#">
+                                        <i class="bi bi-trash icon_color"></i>
+                                    </a>
+                                </td>
+                            <tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
