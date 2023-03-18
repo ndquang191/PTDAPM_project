3acd54591a55fa763d8b4306c603e2cbcd2c942d
nhat <phamminhnhat0208@gmail.com>
2023-03-18 14:08:22 +0700

    fix + them chuc nang

---
+++ public/js/salary/salary.js
@@ -0,0 +1,30 @@
+function searchSalary() {
+    var input, filter, table, tr, td, i, txtValue, e, giaTri, stt;
+    e = document.getElementById("chooseSearch");
+    giaTri = e.options[e.selectedIndex].text;
+    input = document.querySelector(".input-search");
+    filter = input.value.toUpperCase();
+    console.log(filter);
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    if (giaTri == "Tên nhân viên") {
+        stt = 1;
+    }
+    if (giaTri == "Chức vụ") {
+        stt = 2;
+    }
+    if (giaTri == "Phòng ban") {
+        stt = 3;
+    }
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[stt];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
